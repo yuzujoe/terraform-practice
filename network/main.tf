090a3cf682db84ee130fb19cf0cbb6fe32334328
yuzujoe <yuzun225@gmail.com>
2020-04-07 23:06:53 +0900

    private network

--- network/main.tf
+++ network/main.tf
@@ -61,3 +61,23 @@ resource "aws_route_table_association" "public" {
   route_table_id = aws_route_table.public.id
 }
 
+##################
+# private subnet #
+##################
+
+resource "aws_subnet" "private" {
+  vpc_id = aws_vpc.example.id
+  # cidrを被らないようにする
+  cidr_block              = "10.0.64.0/24"
+  availability_zone       = "ap-northeast-1a"
+  map_public_ip_on_launch = false
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.example.id
+}
+
+resource "aws_route_table_association" "private" {
+  subnet_id      = aws_subnet.private.id
+  route_table_id = aws_route_table.private.id
+}
