d8fa55bf8e417d3807ff09b4b994879fc4db1b07
yuzujoe <yuzun225@gmail.com>
2020-04-07 23:37:35 +0900

    Multi AZ

--- network/main.tf
+++ network/main.tf
@@ -18,13 +18,21 @@ resource "aws_vpc" "example" {
 # public subnet #
 #################
 
-resource "aws_subnet" "public" {
+resource "aws_subnet" "public_0" {
   vpc_id     = aws_vpc.example.id
-  cidr_block = "10.0.0.0/24"
+  cidr_block = "10.0.1.0/24"
   # public ipアドレスを自動で割り当ててくれる
   map_public_ip_on_launch = true
   availability_zone       = "ap-northeast-1a"
 }
+
+resource "aws_subnet" "public_1" {
+  vpc_id                  = aws_vpc.example.id
+  cidr_block              = "10.0.2.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "ap-northeast-1c"
+}
+
 
 ####################
 # internet gateway #
@@ -56,46 +64,94 @@ resource "aws_route" "public" {
 # route table association    #
 ##############################
 # 関連付けを忘れた場合defaultのroot tableが使われる
-resource "aws_route_table_association" "public" {
-  subnet_id      = aws_subnet.public.id
-  route_table_id = aws_route_table.public.id
+resource "aws_route_table_association" "public_0" {
+  subnet_id      = aws_subnet.public_0.id
+  route_table_id = aws_route_table.public_0.id
+}
+
+resource "aws_route_table_association" "public_1" {
+  subnet_id      = aws_subnet.public_1.id
+  route_table_id = aws_route_table.public_1.id
 }
 
 ##################
 # private subnet #
 ##################
 
-resource "aws_subnet" "private" {
+resource "aws_subnet" "private_0" {
   vpc_id = aws_vpc.example.id
   # cidrを被らないようにする
-  cidr_block              = "10.0.64.0/24"
+  cidr_block              = "10.0.65.0/24"
   availability_zone       = "ap-northeast-1a"
   map_public_ip_on_launch = false
 }
 
-resource "aws_route_table" "private" {
+resource "aws_subnet" "privat_1" {
+  vpc_id = aws_vpc.example.id
+  # cidrを被らないようにする
+  cidr_block              = "10.0.66.0/24"
+  availability_zone       = "ap-northeast-1c"
+  map_public_ip_on_launch = false
+}
+
+resource "aws_route_table" "private_0" {
   vpc_id = aws_vpc.example.id
 }
 
-resource "aws_route_table_association" "private" {
-  subnet_id      = aws_subnet.private.id
-  route_table_id = aws_route_table.private.id
+resource "aws_route_table" "private_1" {
+  vpc_id = aws_vpc.example.id
+}
+
+resource "aws_route_table_association" "private_0" {
+  subnet_id      = aws_subnet.private_0.id
+  route_table_id = aws_route_table.private_0.id
+}
+
+resource "aws_route_table_association" "private_1" {
+  subnet_id      = aws_subnet.private_1.id
+  route_table_id = aws_route_table.private_1.id
+}
+
+#######
+# eip #
+#######
+
+resource "aws_eip" "nat_gateway_0" {
+  vpc        = true
+  depends_on = [aws_internet_gateway.example]
+}
+
+resource "aws_eip" "nat_gateway_1" {
+  vpc        = true
+  depends_on = [aws_internet_gateway.example]
 }
 
 ##################
 #   nat gateway  #
 ##################
 
-resource "aws_nat_gateway" "example" {
-  allocation_id = aws_eip.nat.id
-  subnet_id     = aws_subnet.public.id
-  depends_on    = [aws-aws_internet_gateway.example]
+resource "aws_nat_gateway" "nat_gateway_0" {
+  allocation_id = aws_eip.nat_gateway_0.id
+  subnet_id     = aws_subnet.public_0.id
+  depends_on    = [aws_internet_gateway.example]
 }
 
-resource "aws_route" "private" {
-  route_table_id = aws_route_table.private.id
+resource "aws_nat_gateway" "nat_gateway_1" {
+  allocation_id = aws_eip.nat_gateway_1.id
+  subnet_id     = aws_subnet.public_1.id
+  depends_on    = [aws_internet_gateway.example]
+}
+
+resource "aws_route" "private_0" {
+  route_table_id = aws_route_table.private_0.id
   # privateではnat_gateway_idを使用する
-  nat_gateway_id         = aws_nat_gateway.example.id
+  nat_gateway_id         = aws_nat_gateway.nat_gateway_0.id
   destination_cidr_block = "0.0.0.0/0"
 }
 
+resource "aws_route" "private_1" {
+  route_table_id = aws_route_table.private_1.id
+  # privateではnat_gateway_idを使用する
+  nat_gateway_id         = aws_nat_gateway.nat_gateway_1.id
+  destination_cidr_block = "0.0.0.0/0"
+}
