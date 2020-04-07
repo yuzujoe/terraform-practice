5b13f81846758d9bfb9bd83a8e51e7ddb20a4d44
yuzujoe <yuzun225@gmail.com>
2020-04-07 23:56:07 +0900

    security group

--- main.tf
+++ main.tf
@@ -4,3 +4,11 @@ module "describe_regions_for_ec2" {
   identifier = "ec2.amazonaws.com"
   policy     = data.aws_iam_policy_document.allow_describe_regions.json
 }
+
+module "example_sg" {
+  source      = "./security-group"
+  name        = "module-sg"
+  vpc_id      = aws_vpc.example.id
+  port        = 80
+  cidr_blocks = ["0.0.0.0/0"]
+}
