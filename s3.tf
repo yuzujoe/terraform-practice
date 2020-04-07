3f3bb8178a7517d23c507e8d8c6f5648b0bdcd1a
yuzujoe <yuzun225@gmail.com>
2020-04-07 09:34:50 +0900

    s3 public bucket

--- s3.tf
+++ s3.tf
@@ -24,3 +24,14 @@ resource "aws_s3_bucket_public_access_block" "private" {
   restrict_public_buckets = true
 }
 
+resource "aws_s3_bucket" "public" {
+  bucket = "public-pragmatic-terraform"
+  acl    = "public-read"
+
+  cors_rule {
+    allowed_origins = ["https://example.com"]
+    allowed_methods = ["GET"]
+    allowed_headers = ["*"]
+    max_age_seconds = 3000
+  }
+}
