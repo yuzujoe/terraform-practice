ed41c2a84e58d3b1a57713dbb401f60006b4e04a
yuzujoe <yuzun225@gmail.com>
2020-04-08 10:44:04 +0900

    alb security group

--- alb/main.tf
+++ alb/main.tf
@@ -27,3 +27,51 @@ resource "aws_lb" "example" {
 output "alb_dns_name" {
   value = aws_lb.example.dns_name
 }
+
+########################
+#  alb security module #
+########################
+
+module "http_sg" {
+  source      = "../security-group"
+  name        = "http-sg"
+  vpc_id      = aws_vpc.example.id
+  port        = 80
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
+module "https_sg" {
+  source      = "../security-group"
+  name        = "https-sg"
+  vpc_id      = aws_vpc.example.id
+  port        = 443
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
+module "http_redirect_sg" {
+  source      = "../security-group"
+  name        = "http-redirect-sg"
+  vpc_id      = aws_vpc.example.id
+  port        = 8080
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
+################
+#  alb lisnter #
+################
+
+resource "aws_lb_lisnter" "http" {
+  load_balancer_arn = aws_lb.example.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+
+    fixed-response {
+      content_type = "text/plan"
+      message_body = "これはHTTPです"
+      status_code  = "200"
+    }
+  }
+}
