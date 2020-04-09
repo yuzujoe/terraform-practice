ca64cd3763f713f374b695859a22c3a8f80bfa4c
yuzujoe <yuzun225@gmail.com>
2020-04-09 09:12:51 +0900

    use https access

--- part8/listener.tf
+++ part8/listener.tf
@@ -13,3 +13,38 @@ resource "aws_lb_listener" "http" {
     }
   }
 }
+
+resource "aws_lb_listener" "https" {
+  load_balancer_arn = aws_lb.example.arn
+  port              = "443"
+  protocol          = "HTTPS"
+  certificate_arn   = aws_acm_certificate.arn
+  ssl_policy        = "ELBSecrityPolicy-2016-08"
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plan"
+      message_body = "これはHTTPSです"
+      status_code  = "200"
+    }
+  }
+}
+
+resource "aws_lb_listener" "redirect_http_to_https" {
+  load_balancer_arn = aws_lb.example.arn
+  port              = "8080"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
+
