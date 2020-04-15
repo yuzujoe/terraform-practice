11c4755b948fc795d86742dcac3cabececb1bb7b
yuzujoe <yuzun225@gmail.com>
2020-04-15 09:44:40 +0900

    ecs service

--- part9/main.tf
+++ part9/main.tf
@@ -4,12 +4,51 @@ resource "aws_ecs_cluster" "example" {
 
 resource "aws_ecs_task_definition" "example" {
   container_definitions = file("./container_definitions.json")
-  family = "example"
-//  cpuに256を指定した場合memoryは512など組み合わせは決まっている
-//  https://docs.aws.amazon.com/ja_jp/AmazonECS/latest/developerguide/task-cpu-memory-error.html
-  cpu = "256"
+  family                = "example"
+  //  cpuに256を指定した場合memoryは512など組み合わせは決まっている
+  //  https://docs.aws.amazon.com/ja_jp/AmazonECS/latest/developerguide/task-cpu-memory-error.html
+  cpu    = "256"
   memory = "512"
-//  fargateの場合はネットワークモードをawsvpcにする
-  network_mode = "awsvpc"
+  //  fargateの場合はネットワークモードをawsvpcにする
+  network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
+}
+
+resource "aws_ecs_service" "example" {
+  name                              = "example"
+  cluster                           = aws_ecs_cluster.example.arn
+  task_definition                   = aws_ecs_task_definition.example.arn
+//  ECSが維持するタスク数
+  desired_count                     = 2
+  launch_type                       = "FARGATE"
+  platform_version                  = "1.3.0"
+  health_check_grace_period_seconds = 60
+
+  network_configuration {
+    assign_public_ip = false
+    security_groups  = [module.nginx_sg.security_group_id]
+
+    subnets = [
+      aws_subnet.private_0.id,
+      aws_subnet.private_1.id,
+    ]
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.example.arn
+    container_name   = "example"
+    container_port   = 80
+  }
+
+  lifecycle {
+    ignore_changes = [task_definition]
+  }
+}
+
+module "nginx_sg" {
+  source       = "./security_group"
+  name         = "nginx_sg"
+  vpc_id       = aws_vpc.example.id
+  port         = 80
+  cider_blocks = [aws_vpc.example.cider_block]
 }
