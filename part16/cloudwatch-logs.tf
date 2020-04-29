d544a6b0c35f8a111abdfb31071e3f71ba4714ef
yuzujoe <yuzun225@gmail.com>
2020-04-29 09:21:50 +0900

    cloudwatch logs subscription filter

--- part16/cloudwatch-logs.tf
+++ part16/cloudwatch-logs.tf
@@ -1,13 +1,21 @@
 data "aws_iam_policy_document" "cloudwatch_logs" {
   statement {
-    effect = "Allow"
-    actions = ["firehose:*"]
+    effect    = "Allow"
+    actions   = ["firehose:*"]
     resources = ["arn:aws:firehose:ap-northeast-1:*:*"]
   }
 
   statement {
-    effect = "Allow"
-    actions = ["iam:PassRole"]
+    effect    = "Allow"
+    actions   = ["iam:PassRole"]
     resources = ["arn:aws:iam:*:role/cloudwatch-logs"]
   }
+}
+
+resource "aws_cloudwatch_log_subscription_filter" "example" {
+  destination_arn = aws_kinesis_firehose_delivery_stream.example.arn
+  filter_pattern  = "[]"
+  log_group_name  = aws_cloudwatch_log_group.for_ecs_scheduled_task.name
+  name            = "example"
+  role_arn        = module.cloudwatch_logs_role.iam_role_arn
 }
